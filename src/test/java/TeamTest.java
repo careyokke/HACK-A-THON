6c635b3bf75b4c724a551df0314044baefbdf191
Kong <kongchin@amazon.com>
2017-07-07 15:34:33 -0700

    add Testings

--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -7,3 +7,24 @@ public class TeamTest {
   public void tearDown() {
     Team.clear();
   }
+
+
+  @Test
+  public void Team_instantiatesCorrectly_true() {
+    Team testTeam = new Team("DREAM TEAM");
+    assertEquals(true, testTeam instanceof Team);
+  }
+
+  @Test
+public void getTeamName_teamInstantiatesWithName_ITCrowd() {
+  Team testTeam = new Team("DREAM TEAM");
+  assertEquals("DREAM TEAMd", testTeam.getTeamName());
+}
+
+@Test
+public void all_returnsAllInstancesOfTeam_true() {
+  Team firstTeam = new Team("DREAM TEAM");
+  Team secondTeam = new Team("Wanna BE TEAM");
+  assertEquals(true, Team.all().contains(firstTeam));
+  assertEquals(true, Team.all().contains(secondTeam));
+}
