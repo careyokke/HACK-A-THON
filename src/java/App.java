4babf93fabf081318f4585a47c7f93359f861ab0
Kong <kongchin@amazon.com>
2017-07-07 15:18:03 -0700

    adding public and public class to Team.java

--- src/java/App.java
+++ src/java/App.java
@@ -6,43 +6,45 @@ import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
-public class App {
+  public class App {
   public static void main(String[] args) {
-    staticFileLocation("/public");
-    String layout = "templates/layout.vtl";
+      staticFileLocation("/public");
+      String layout = "templates/layout.vtl";
 
-    get("/members/:id", (request, response) -> {
-          HashMap<String, Object> model = new HashMap<String, Object>();
-          Member member = Member.find(Integer.parseInt(request.params(":id")));
-          model.put("member", member);
-          model.put("template", "templates/member.vtl");
-          return new ModelAndView(model, layout);
-        }, new VelocityTemplateEngine());
+      get("/members/:id", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        Member member = Member.find(Integer.parseInt(request.params(":id")));
+        model.put("member", member);
+        model.put("template", "templates/member.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
 
-        get("/members", (request, response) -> {
-  Map<String, Object> model = new HashMap<String, Object>();
-  model.put("members", Member.all());
-  model.put("template", "templates/members.vtl");
-  return new ModelAndView(model, layout);
-}, new VelocityTemplateEngine());
+      get("/members", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("members", Member.all());
+        model.put("template", "templates/members.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
 
-post("/members", (request, response) -> {
-  Map<String, Object> model = new HashMap<String, Object>();
-  String memberName = request.queryParams("memberName");
-  Member newMember = new Member(memberName);
-  model.put("template", "templates/success.vtl");
-  return new ModelAndView(model, layout);
-}, new VelocityTemplateEngine());
+      post("/members", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        String memberName = request.queryParams("memberName");
+        Member newMember = new Member(memberName);
+        model.put("template", "templates/success.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
 
-get("/", (request, response) -> {
-    Map<String, Object> model = new HashMap<String, Object>();
-    //model.put("members", request.session().attribute("members"));
-    model.put("template", "templates/index.vtl");
-    return new ModelAndView(model, layout);
-  }, new VelocityTemplateEngine());
+      get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      //model.put("members", request.session().attribute("members"));
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
-  get("members/new", (request, response) -> {
-    Map<String, Object> model = new HashMap<String, Object>();
-    model.put("template", "templates/member-form.vtl");
-    return new ModelAndView(model, layout);
-  }, new VelocityTemplateEngine());
+      get("members/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/member-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
+}
--- src/java/Team.java
+++ src/java/Team.java
@@ -7,3 +7,38 @@ public class Team {
   private static List<Team> instances = new ArrayList<Team>();
   private String mTeamName;
   private List<Member> mMembers;
+
+  public Team(String teamName) {
+      mId = instances.size();
+      instances.add(this);
+      mTeamName = teamName;
+      mMembers = new ArrayList<Member>();
+    }
+
+    public void addMember(Member member) {
+      mMembers.add(member);
+    }
+    public String getTeamName() {
+    return mTeamName;
+    }
+
+    public static List<Team> all() {
+      return instances;
+    }
+
+    public static void clear() {
+      instances.clear();
+    }
+
+    public int getId() {
+      return mId;
+    }
+
+    public static Team find(int id) {
+      return instances.get(id -1);
+    }
+
+    public List<Member> getMembers() {
+      return mMembers;
+    }
+  }
