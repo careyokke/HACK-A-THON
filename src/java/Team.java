744b88a02f5634e124f794e6a6559ef4a6a2e8ed
Kong <kongchin@amazon.com>
2017-07-07 15:12:27 -0700

    adding public and public class to Team.java

---
+++ src/java/Team.java
@@ -0,0 +1,9 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Team {
+  private int mId;
+  private String mTeamName;
+  private static List<Team> instances = new ArrayList<Team>();
+  private String mTeamName;
+  private List<Member> mMembers;
