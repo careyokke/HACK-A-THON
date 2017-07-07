fcbcac3a311bc9eedbd28f0335b6525d7c544acb
Kong <kongchin@amazon.com>
2017-07-07 15:06:10 -0700

    Import all java and spark inside App.java

---
+++ src/java/App.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
